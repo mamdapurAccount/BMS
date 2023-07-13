7e7f0a8709d6516f68bb7822a04f64e4dd597dc2
mamdapurAccount <56784976+mamdapurAccount@users.noreply.github.com>
2023-07-13 10:51:56 +0530

    Thakur

--- src/main/java/com/infosys/Test.java
+++ src/main/java/com/infosys/Test.java
@@ -3,7 +3,7 @@ package com.infosys;
 public class Test {
 
 	public static void main(String[] args) {
-	System.out.println("rishikesh needs lots of patience");
+	System.out.println("rishikesh needs lots of patience and practice");
 
 	}
 
